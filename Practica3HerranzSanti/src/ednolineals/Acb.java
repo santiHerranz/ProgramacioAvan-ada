4118097e4c7dd7fba1a906d8667db93ee501684c
santi herranz <santi.herranz@gmail.com>
2015-10-28 23:26:21 +0100

    Inici Practica3

---
+++ Practica3HerranzSanti/src/ednolineals/Acb.java
@@ -0,0 +1,21 @@
+package ednolineals;
+
+public interface Acb<E extends Comparable<E>> extends Ab<E> {
+
+	/*
+	 * llença una excepció si l’element que s’insereix està repetit
+	 */
+	public void inserir (E e) throws ArbreException;
+	
+	/*
+	 * @return retorna true si ha trobat l'element i l'ha esborrat
+	 * 		   ull!!!! retorna false en cas contrari 
+	 */
+	public boolean esborrar (E e);
+
+	/*
+	 * @return retorna true si l’arbre conté un element com el donat com a paràmetre
+	 */
+	public boolean membre (E e);
+	
+}
---
+++ Practica3HerranzSanti/src/ednolineals/ArbreException.java
@@ -0,0 +1,10 @@
+package ednolineals;
+
+public class ArbreException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6258603585349928223L;
+
+}
