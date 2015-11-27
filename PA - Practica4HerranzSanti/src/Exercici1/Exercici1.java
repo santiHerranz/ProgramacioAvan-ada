0ece98f9e567b4c6158b2c3d074a8044dbb11d04
santi herranz <santi.herranz@gmail.com>
2015-11-27 01:38:52 +0100

    Entrega

--- PA - Practica4HerranzSanti/src/Exercici1/Exercici1.java
+++ PA - Practica4HerranzSanti/src/Exercici1/Exercici1.java
@@ -3,29 +3,19 @@ package Exercici1;
 import java.util.Scanner;
 
 public class Exercici1 {
-
-	
 	/*
-	 * Entrada:
-	 * Ciutat origen
-	 * Ciutat destí
-	 * Quilómetres de distància que pot recòrrer amb el dipósit ple
-	 * Nombre de gasolineres
-	 * Distancia entre gasolineres
-	 *  
 	 * Quins són els candidats del problema?
-	 *  Les gasolineres per reposar combustible
+	 *  Totes les gasolineres del recorregut
 	 * 
 	 * Quina funció de selecció aplicarà el vostre algorisme?
-	 *  El viatjant ha de repostar sempre que la propera gasolinera estiqui més lluny que els quilómetres
-	 *  que li queden de bezina al dipósit
+	 *  El viatjant ha de repostar sempre que al diposit no li quedin més quilómetres per arribar a la següent gasolinera 
 	 * 
 	 * La vostra funció de selecció, garanteix trobar sempre la millor solució?
-	 *  No sempre, perque pot haver dues  
+	 *  Si
 	 * 
 	 * Perquè?
-	 * 
-	 * Análisi descendent
+	 *  L'enunciat garanteix que les distancies a les gasolineres son sempre més petites que la capacitat
+	 *  del dipósit i la funció de selecció fa que no s'aturi si pot arribar a la següent gasolinera
 	 * 
 	 */
 	
@@ -35,36 +25,69 @@ public class Exercici1 {
 		String ciutat_origen = "";
 		String ciutat_desti = "";
 		int desti = 0;
-		int TAMANY_DIPOSIT = 0;
+		int tamany_diposit = 0;
 		int numero_gasolineres = 0;
 		Gas gasolineres[] = null;
 		
 		Scanner reader = new Scanner(System.in);
 		
-		System.out.println("Entrada AUTOMÀTICA(A) o qualsevol altra tecla per MANUAL:");
+		System.out.println("Menú:");
+		System.out.println("  1 - Aleatori");
+		System.out.println("  2 - Predefinit");
+		System.out.println("  3 - Manual");
+		System.out.println("Tria una opció:");
 		String opcio = reader.next();
-		if(opcio.equals("A") || opcio.equals("a")) {
+		opcio = opcio.toLowerCase();
+
+
+		if(opcio.equals("1")) {
+			System.out.println("1 - Aleatori");
+
 			ciutat_origen = "Barcelona";
 			ciutat_desti = "Mataró";
-			TAMANY_DIPOSIT = 150;
-			
-			Gas[] gasolineresAuto = {
-					new Gas("Sortida",0),
-					new Gas("1",(int)(Math.random()*TAMANY_DIPOSIT)+1),
-					new Gas("2",(int)(Math.random()*TAMANY_DIPOSIT)+1),
-					new Gas("3",(int)(Math.random()*TAMANY_DIPOSIT)+1),
-					new Gas("4",(int)(Math.random()*TAMANY_DIPOSIT)+1),
-					new Gas("5",(int)(Math.random()*TAMANY_DIPOSIT)+1),
-					new Gas("6",(int)(Math.random()*TAMANY_DIPOSIT)+1),
-					new Gas("7",(int)(Math.random()*TAMANY_DIPOSIT)+1)
-					};	
+			tamany_diposit = 100+((int)(Math.random()*100)+1);
+
+			numero_gasolineres = 1+(int)(Math.random()*10);
+			Gas gasolineresAuto[] = new Gas[numero_gasolineres+1];
+			gasolineresAuto[0] = new Gas("Sortida",0);
+			for(int i=1; i<=numero_gasolineres;i++)
+				gasolineresAuto[i] = new Gas("Gas "+ Character.toString ((char) ('A'+i-1)),(int)(Math.random()*tamany_diposit)+1);
+
 			gasolineres = gasolineresAuto;
 			numero_gasolineres = gasolineres.length;
 			
 			for(Gas g: gasolineres)
 				desti += g.km;
+			
+			desti += (int)(Math.random()*tamany_diposit)+1;
 		}
 		
+		if(opcio.equals("2")) {
+			System.out.println("2 - Predefinit");
+
+			ciutat_origen = "Barcelona";
+			ciutat_desti = "Mataró";
+			tamany_diposit = 50;
+
+			Gas gasolineresPredefinit[] = {
+					new Gas("Sortida",0),
+					new Gas("Gas A", 40),
+					new Gas("Gas B", 20),
+					new Gas("Gas C", 10),
+					new Gas("Gas D", 20)
+					};				
+			gasolineres = gasolineresPredefinit;
+			numero_gasolineres = gasolineres.length;
+			
+			for(Gas g: gasolineres)
+				desti += g.km;
+			
+			desti += 10;
+		}		
+
+		if(opcio.equals("3")) {
+			System.out.println("3 - Manual");
+		}
 
 		while(ciutat_origen == "") {
 			System.out.println("Entra la ciutat origen:");
@@ -76,108 +99,181 @@ public class Exercici1 {
 			ciutat_desti = reader.next();
 		} ;
 		
-		while(TAMANY_DIPOSIT == 0) {
+		while(tamany_diposit == 0) {
 			System.out.println("Entra el tamany del dipósit:");
 			String value = reader.next();
 			try{
-				TAMANY_DIPOSIT = Integer.parseInt(value);
+				tamany_diposit = Integer.parseInt(value);
+			} catch(NumberFormatException e) {
+				System.out.println("Error: El tamany del dipósit ha de ser numèric");
 			} catch(Exception e) {
 				System.out.println(e.getMessage());
 			}
 		};
-		
 
 		while(numero_gasolineres == 0) {
-			System.out.println("Entra el número de gasolineres:");
+			System.out.println("Entra la quantitat de gasolineres al recorregut:");
 			String value = reader.next();
 			try{
 				numero_gasolineres = Integer.parseInt(value);
+			} catch(NumberFormatException e) {
+				System.out.println("Error: La quantitat de gasolineres ha de ser numèrica");
 			} catch(Exception e) {
 				System.out.println(e.getMessage());
 			}
 		} ;
 
 		if(gasolineres == null) {
-			gasolineres = new Gas[numero_gasolineres];
-			for(int i=0; i<numero_gasolineres;i++){
+			gasolineres = new Gas[numero_gasolineres+1];
+			gasolineres[0] = new Gas("Sortida",0);
+			for(int i=1; i<numero_gasolineres+1;i++){
 				do {
 					System.out.println(String.format("Entra la distancia a la gasolinera %s:", i));
 					String value = reader.next();
 					try{
-						gasolineres[i] = new Gas("Gas "+ Character.toString ((char) ('A'+i-1)), Integer.parseInt(value));
+						int km = Integer.parseInt(value);
+						if(km>tamany_diposit)
+							throw new Exception(String.format("La distancia no pot ser superior a tamany diposit (%d)", tamany_diposit));
+						if(km<1)
+							throw new Exception("Error: La distancia no pot ser inferior a 1");
+						gasolineres[i] = new Gas("Gas "+ Character.toString ((char) ('A'+i-1)), km);
+					} catch(NumberFormatException e) {
+						System.out.println("Error: La distancia ha de ser numèrica");
 					} catch(Exception e) {
 						System.out.println(e.getMessage());
 					}
 				} while(gasolineres[i] == null);
 			}
+
+			int acum = 0;
+			for(int i=0; i<gasolineres.length; i++) {
+				acum += gasolineres[i].km;
+			}
+			
+			System.out.println("Entra la distancia al destí:");
+			String value = reader.next();
+			try{
+				int km = Integer.parseInt(value);
+				if(km>tamany_diposit)
+					throw new Exception(String.format("La distancia no pot ser superior a tamany diposit (%d)", tamany_diposit));
+				if(km<1)
+					throw new Exception("Error: La distancia no pot ser inferior a 1");
+				desti = acum + km;
+			} catch(NumberFormatException e) {
+				System.out.println("Error: La distancia ha de ser numèrica");
+			} catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+
 		}
 		
 		//Parades posibles
-		Gas [] Parades=new Gas[gasolineres.length];
+		Gas parades[] = new Gas[gasolineres.length];
 
+		System.out.println("\n---Dades-------------");
+		System.out.println("Ciutat origen: "+ ciutat_origen);
+		System.out.println("Ciutat destí: "+ ciutat_desti);
+		System.out.println("Distancia al destí: "+ desti +" km");
+		
 		int resultat;
-		int distanciaDeposit = TAMANY_DIPOSIT;
-		System.out.println("distanciaDeposit: "+ distanciaDeposit +" km");
+		int distanciaDeposit = tamany_diposit;
+		System.out.println("Tamany depósit: "+ distanciaDeposit +" km");
 
 		int acum = 0;
 		int delta = 0;
-		for(int i=0; i<gasolineres.length-1; i++) {
+		for(int i=0; i<gasolineres.length; i++) {
 			delta= gasolineres[i].km;
 			acum += delta;
-			System.out.println(i+1+" - "+ gasolineres[i].nom +" \ta  "+ gasolineres[i].km +" km \t acum: "+ acum +" km");
+			System.out.println(i+" - "+ gasolineres[i].nom 
+					+"\t acum:"+ acum +" km"
+					+"\t a "+ gasolineres[i].km +" km"
+					);
 		}
-		System.out.println(gasolineres.length+" - Desti \ta  "+ (desti-acum) +" km \t acum: "+ desti +" km");
-		System.out.println("Desti: "+ desti +" km");
+		System.out.println(gasolineres.length +" - Desti"
+				+"\t acum:"+ desti +" km"
+				+"\ta "+ (desti-acum) +" km"
+				);
 		
-		resultat=calcula_parades(distanciaDeposit, gasolineres, Parades, desti);
-
+		System.out.println("\n---Càlcul amb técnica voraç -------------");
+		resultat=calcula_aturades(distanciaDeposit, gasolineres, parades, desti);
 		
 		System.out.println("\n---Resultat-------------");
 		if (resultat!=0){
 			System.out.println("Aturades: "+ (resultat-1) +" de "+ gasolineres.length);
-			for(int i=0; i<resultat; i++)
-				System.out.println(i+" - "+ Parades[i].nom);
-			System.out.println(resultat+ " - Destí");
+			for(int i=1; i<resultat; i++)
+				System.out.println(i+" - "+ parades[i].nom);
 		} else 
 			System.out.println("No existeix solucio");
 	}
 	
-	private static int calcula_parades(int distanciaDeposit, Gas []Candidats, Gas []Solucio, int desti){
+	
+	private static int calcula_aturades(int kmDiposit, Gas Candidats[], Gas Solucio[], int desti){
 			int resultat=0;
-			int recorregut = 0; 						// acumulat solució
-			int diposit = distanciaDeposit;				// Reposta
-
-			
+			int kmAcumulats = 0;
 			// Els candidats venen en seqüència del recorregut
-			int index = 0; //índex del següent candidat a considerar
-			Solucio[resultat++] = Candidats[index++];	// Gasolinera de sortida
-			while (index < Candidats.length-2){ // Valorem tots els candidats
-				recorregut += Candidats[index].km;
+			int index = 0; // índex del candidat a considerar
+			
+			// Omplir dipósit
+			int diposit = kmDiposit;
+			
+			System.out.println(index+" - "+ Candidats[index].nom 
+					+"\tacum:"+ kmAcumulats +" km"
+					+"\tdiposit: queden "+ diposit +" km"
+					+"\tsegüent a "+ Candidats[index+1].km +" km" );			
+
+			Solucio[resultat++] = Candidats[index++];	// afegir la gasolinera de sortida a la solució
+
+			// Estructura voraç
+			while (index < Candidats.length) { // mentre hi hagi candidats
 				
-				System.out.print(Candidats[index].nom +" -\t acum: "+ recorregut +" \t diposit: "+ (diposit-Candidats[index].km) +" km \t seguent a "+ Candidats[index+1].km +" km" );
+				int distancia_actual = Candidats[index].km;
+				int distancia_seguent = 0;
+
+				// descomptem la distancia actual al dipósit
+				diposit -= distancia_actual;
+
+				// Acumulem la distància actual
+				kmAcumulats += distancia_actual;
+
+				if(index == Candidats.length-1) // Cas especial ultima gasolinera
+					distancia_seguent = desti-kmAcumulats;
+				else
+					distancia_seguent = Candidats[index+1].km;
 				
-				if (Candidats[index].km+Candidats[index+1].km < diposit){
-					System.out.println("");
-					diposit -= Candidats[index].km;
-				} else {
-					System.out.println(" - Aturada");
+				System.out.print(index+" - "+ Candidats[index].nom 
+						+"\tacum:"+ kmAcumulats +" km"
+						+"\tdiposit: queden "+ diposit +" km"
+						+"\tsegüent a "+ distancia_seguent +" km" );
+
+				// funció de selecció: 
+				//	 Si el dipósit no té prou gasolina per arribar a la següent gasolinera haig d'aturar i omplir el dipósit
+				if (diposit < distancia_seguent ){
+					System.out.print(" - ATURADA");
+					// Omplir dipósit
+					diposit = kmDiposit;
+					// Afegir a la solució
 					Solucio[resultat++] = Candidats[index];
-					diposit = distanciaDeposit; 		// Reposta
+				} else {
+					System.out.print(" - sense aturada");
 				}
+				
 				index++;
+				System.out.println("");
 			} 
 
-			// Cas ultima gasolinera abans del destí
-			recorregut += Candidats[index].km;
-			System.out.print(Candidats[index].nom +" -\t acum: "+ recorregut +" \t diposit: "+ (diposit-Candidats[index].km) +" km  \t desti a "+ (desti-recorregut) +" km" );
-			if (Candidats[index].km + desti-recorregut < diposit){
-				System.out.println("");
-				diposit -= Candidats[index].km;
-			} else {
-				System.out.println(" - Aturada");
-				Solucio[resultat++] = Candidats[index];
-				diposit = distanciaDeposit;				// Reposta
-			}
+			// Últim tram fins al destí
+			int distancia_actual = desti-kmAcumulats;
+			
+			// descomptem la distancia actual al dipósit
+			diposit -= distancia_actual;
+
+			// Acumulem la distància actual
+			kmAcumulats += distancia_actual;
+			
+			System.out.print(index+" - Destí" 
+					+"\tacum:"+ kmAcumulats +" km"
+					+"\tdiposit: queden "+ diposit +" km"
+					);
 			
 			return resultat;
 	} 
--- PA - Practica4HerranzSanti/src/Exercici2/Exercici2.java
+++ PA - Practica4HerranzSanti/src/Exercici2/Exercici2.java
@@ -1,88 +1,182 @@
 package Exercici2;
 
+import java.util.Scanner;
+
+import Exercici1.Gas;
+
 public class Exercici2 {
 
 
 	public static void main(String[] args) {
 
 		System.out.println("Exercici2 - Tècnica del divideix i venç - Polvorons");
+		Scanner reader = new Scanner(System.in);
 		
+		System.out.println("Menú:");
+		System.out.println("  1 - Aleatori");
+		System.out.println("  2 - Predefinit");
+		System.out.println("  3 - Manual");
+		System.out.println("  4 - Megacaixa");
+		System.out.println("Tria una opció:");
+		String opcio = reader.next();
+		opcio = opcio.toLowerCase();
 		
-		System.out.println("\n\n**********\nCaixa manual\n**********");
-		
-        int[] caixaManual = {
-        		Tipus.VAINILLA.intValue(),
-//        		Tipus.COCO.intValue(),
-//        		Tipus.ATMELLA.intValue(),
-        		Tipus.VAINILLA.intValue(),
-        		Tipus.XOCOLATA.intValue(),
-        		Tipus.VAINILLA.intValue(),
-        		Tipus.LLIMONA.intValue(),
-        		Tipus.VAINILLA.intValue(),
-        		Tipus.VAINILLA.intValue()
-        		};
 
-        
-		Caixa novaCaixa = new Caixa(caixaManual);
-		novaCaixa.explicar=true;
-		
-		System.out.println(novaCaixa.toString());
-		
-        int element = novaCaixa.esAvorrida(); // obtenir element majoritari amb la técnica divideix i venç
-        if(element==-1)
-            System.out.println("La caixa no és avorrida");
-        else
-            System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element) + "");
-		
-        //System.exit(0);
+		if(opcio.equals("1")) {
+			System.out.println("1 - Aleatori");
 
-        
-		System.out.println("\n\n**********\nPalet de Caixes aleatories\n**********");
+			System.out.println("\n\n**********\nPalet de Caixes aleatories\n**********");
 
-        // Omplir caixes
-		int caixesPalet = (int)(10 * Math.random())+4;
-        Caixa[] palet = new Caixa[caixesPalet];
-        for(int i=0; i<palet.length; i++){
-           int polvoronsCaixa = (int)(10 * Math.random())+3;
-     	   int[] contingut = new int[polvoronsCaixa];
-           for (int j = 0; j < contingut.length; j++) {
-        	   contingut[j] = (int)(3 * Math.random())+1;
-           }        	
-        	palet[i] = new Caixa(contingut);
-        }
-        
-        // Mirar caixes
-        for(int i=0; i < palet.length; i++){
-        	System.out.println();
-    		System.out.println(palet[i].toString());
-        	int element2 = palet[i].esAvorrida();
-   		
-            if(element2==-1)
-                System.out.println("La caixa no és avorrida");
-            else
-                System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element2) + "");
-        }
+	        // Omplir caixes
+			int caixesPalet = (int)(10 * Math.random())+4;
+	        Caixa[] palet = new Caixa[caixesPalet];
+	        for(int i=0; i<palet.length; i++){
+	           int polvoronsCaixa = (int)(10 * Math.random())+3;
+	     	   int[] contingut = new int[polvoronsCaixa];
+	           for (int j = 0; j < contingut.length; j++) {
+	        	   contingut[j] = (int)(3 * Math.random())+1;
+	           }        	
+	        	palet[i] = new Caixa(contingut);
+	        }
+	        
+	        // Mirar caixes
+	        for(int i=0; i < palet.length; i++){
+	        	System.out.println();
+	    		System.out.println(palet[i].toString());
+	        	int element2 = palet[i].esAvorrida();
+	   		
+	            if(element2==-1)
+	                System.out.println("La caixa no és avorrida");
+	            else
+	                System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element2) + "");
+	        }
+	        
+			
+		}
         
 
-		
-        
-		System.out.println("\n\n**********\nCaixa aleatoria molt gran\n**********");
-        
-		Caixa caixa = new Caixa(1000); // MAX 100000000
-		
-        long inici = System.currentTimeMillis();
-        int element3 = caixa.esAvorrida(); // obtenir element majoritari amb la técnica divideix i venç
-        long temps = System.currentTimeMillis() - inici;
+		if(opcio.equals("2")) {
+			System.out.println("2 - Predefinit");
+			
+	        int[] caixaPredefinit = {
+	        		Tipus.VAINILLA.intValue(),
+//	        		Tipus.COCO.intValue(),
+//	        		Tipus.ATMELLA.intValue(),
+	        		Tipus.VAINILLA.intValue(),
+	        		Tipus.XOCOLATA.intValue(),
+	        		Tipus.VAINILLA.intValue(),
+	        		Tipus.LLIMONA.intValue(),
+	        		Tipus.VAINILLA.intValue(),
+	        		Tipus.VAINILLA.intValue()
+	        		};
 
-        if(caixa.contingut.length<=100){
-        	System.out.println(caixa.toString());
-        }
-        System.out.println(caixa.contingut.length);
-        if(element3==-1)
-            System.out.println("La caixa no és avorrida");
-        else
-            System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element3) + "");
-        System.out.println("" + temps + " ms");
+	        
+			Caixa novaCaixa = new Caixa(caixaPredefinit);
+			novaCaixa.explicar=true;
+			
+			System.out.println(novaCaixa.toString());
+			
+	        int element = novaCaixa.esAvorrida(); // obtenir element majoritari amb la técnica divideix i venç
+	        if(element==-1)
+	            System.out.println("La caixa no és avorrida");
+	        else
+	            System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element) + "");
+			
+	        //System.exit(0);			
+		}		
+
+		if(opcio.equals("3")) {		
+			System.out.println("3 - Manual");
+
+			int numero_polvorons = 0;
+			while(numero_polvorons == 0) {
+				System.out.println("Entra la quantitat de polvorons de la caixa:");
+				String value = reader.next();
+				try{
+					numero_polvorons = Integer.parseInt(value);
+					if(numero_polvorons<1)
+						throw new Exception("Error: La quantitat de polvorons no pot ser inferior a 1");
+					
+				} catch(NumberFormatException e) {
+					System.out.println("Error: La quantitat de polvorons ha de ser numèrica");
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+			} ;
+			
+			int[] caixaManual = new int[numero_polvorons];
+			for(int i=0; i<numero_polvorons;i++){
+				do {
+					System.out.println(String.format("Entra el tipus de polvoro %s:", i+1));
+					System.out.println(String.format("%d - %s:", Tipus.VAINILLA.intValue(), Tipus.VAINILLA));
+					System.out.println(String.format("%d - %s:", Tipus.XOCOLATA.intValue(), Tipus.XOCOLATA));
+					System.out.println(String.format("%d - %s:", Tipus.COCO.intValue(), Tipus.COCO));
+					System.out.println(String.format("%d - %s:", Tipus.ATMELLA.intValue(), Tipus.ATMELLA));
+					String value = reader.next();
+					try{
+						int tipus = Integer.parseInt(value);
+						if(tipus != Tipus.VAINILLA.intValue() && tipus != Tipus.XOCOLATA.intValue() &&tipus != Tipus.COCO.intValue() &&tipus != Tipus.ATMELLA.intValue())
+							throw new Exception("Error: Tipus de polvoró no reconegut");
+						caixaManual[i] = tipus;
+					} catch(NumberFormatException e) {
+						System.out.println("Error: El tipus de polvoró ha de ser numèric");
+					} catch(Exception e) {
+						System.out.println(e.getMessage());
+					}
+				} while(caixaManual[i] == 0);
+			}			
+			
+			
+			Caixa novaCaixa = new Caixa(caixaManual);
+			
+			System.out.println(novaCaixa.toString());
+			
+	        int element = novaCaixa.esAvorrida(); // obtenir element majoritari amb la técnica divideix i venç
+	        if(element==-1)
+	            System.out.println("La caixa no és avorrida");
+	        else
+	            System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element) + "");
+			
+		}		
+
+		if(opcio.equals("4")) {
+			System.out.println("4 - Megacaixa");
+
+			int numero_polvorons = 0;
+			while(numero_polvorons == 0) {
+				System.out.println("Entra la quantitat de polvorons de la caixa (prova 1 millió o més):");
+				String value = reader.next();
+				try{
+					numero_polvorons = Integer.parseInt(value);
+					if(numero_polvorons<1)
+						throw new Exception("Error: La quantitat de polvorons no pot ser inferior a 1");
+					
+				} catch(NumberFormatException e) {
+					System.out.println("Error: La quantitat de polvorons ha de ser numèrica");
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+			} ;			
+			
+			System.out.println("Calculant... esperi, si us plau...");
+			Caixa caixa = new Caixa(numero_polvorons); // MAX 100000000
+			
+	        long inici = System.currentTimeMillis();
+	        int element3 = caixa.esAvorrida(); // obtenir element majoritari amb la técnica divideix i venç
+	        long temps = System.currentTimeMillis() - inici;
+
+	        if(caixa.contingut.length<=100){
+	        	System.out.println(caixa.toString());
+	        }
+	        System.out.println(caixa.contingut.length);
+	        if(element3==-1)
+	            System.out.println("La caixa no és avorrida");
+	        else
+	            System.out.println("La caixa és avorrida, element majoritari: " + Tipus.fromInteger(element3) + "");
+	        System.out.println("" + temps + " ms");
+
+		}
+		
         
         
 	
